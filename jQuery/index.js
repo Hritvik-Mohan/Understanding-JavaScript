7570b608f51286e98a174e203bdff51af66aa6d4
Hritvik-Mohan <hritvik5005@gmail.com>
2021-11-15 10:13:25 +0530

    Added jQuery

---
+++ jQuery/index.js
@@ -0,0 +1,29 @@
+// $("button").click(function() {
+//     $("h1").css("color", "purple");
+// });
+
+
+
+// $(document).keydown(function(event) {
+//     $("h1").text(event.key);
+// }); 
+
+
+
+// $("h1").on("mouseover", function() {
+//     $("h1").css("color", "purple");
+// });
+
+// $("h1").before("<button>New</button>");
+
+// $("h1").after("<button>New</button>");
+
+// $("h1").prepend("<button>New</button>");
+
+// $("h1").
+
+
+
+$("button").on("click", function() {
+    $("h1").animate({opacity: 0.5});
+});
